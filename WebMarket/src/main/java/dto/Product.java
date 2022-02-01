693c20a2b6e99a4fbb40dd32bf5e907388e9546e
kim_jeongyeon <wjddus9132@gmail.com>
2022-02-01 11:00:44 +0900

    session

--- WebMarket/src/main/java/dto/Product.java
+++ WebMarket/src/main/java/dto/Product.java
@@ -14,6 +14,7 @@ public class Product implements Serializable{
 	private long unitsInStock;
 	private String condition;
 	private String filename;
+	private int quantity;
 	
 	public Product() {
 		super();
@@ -96,5 +97,13 @@ public class Product implements Serializable{
 	
 	public void setFilename(String filename) {
 		this.filename = filename;
+	}
+	
+	public int getQuantity() {
+		return quantity;
+	}
+	
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
 	}
 }
